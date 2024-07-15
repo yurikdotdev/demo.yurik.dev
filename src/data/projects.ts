ca7f6cc2ebd34db47403fca72a8601d10cd4411f
yurik-dev <172495341+yurik-dev@users.noreply.github.com>
2024-07-15 20:52:57 +0700

    feat: add Works component

---
+++ src/data/projects.ts
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    title: 'Anim Duis',
+    description: 'Anim duis minim elit labore mollit dolor aliqua qui non.',
+    url: ''
+  },
+  {
+    title: 'Minim Elit',
+    description: 'Culpa elit ut ut irure nisi.',
+    url: ''
+  },
+  {
+    title: 'Dolor Aliqua',
+    description: 'Laboris do ipsum enim quis tempor ut proident.',
+    url: ''
+  },
+];
---
+++ src/data/writings.ts
@@ -0,0 +1,21 @@
+export const writings = [
+  {
+    title:
+      'Ipsum occaecat culpa aliquip ullamco cillum veniam ipsum cupidatat adipisicing sunt.',
+    description:
+      'Occaecat consequat duis exercitation adipisicing sunt ipsum voluptate consequat anim ad.',
+    url: 'posts/post-1',
+  },
+  {
+    title:
+      'Deserunt dolor elit dolore dolore ullamco ut adipisicing magna eu adipisicing sunt nisi nisi.',
+    description:
+      'Dolore consectetur elit sunt reprehenderit ex culpa labore commodo nostrud eu.',
+    url: '',
+  },
+  {
+    title: 'Dolor enim enim velit esse dolore non consectetur ex.',
+    description: 'Eu amet elit consectetur reprehenderit aliqua.',
+    url: '',
+  },
+];
