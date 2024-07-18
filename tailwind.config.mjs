e8a611ce313f07790e603d3a2706c45829a3813a
yurik-dev <172495341+yurik-dev@users.noreply.github.com>
2024-07-18 22:17:14 +0700

    feat: implement dark mode toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     colors: {
       'black-bg': '#111111',
       'white-text': '#E5E5E5',
-      'grey-text': '#A0A0A0',
+      'grey-text': '#a9a9a9',
     },
     extend: {},
   },
