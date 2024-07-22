74d06e88944e5f1a896b91f0a689d750521e8997
yurik-dev <172495341+yurik-dev@users.noreply.github.com>
2024-07-22 20:27:30 +0700

    refactor: update global styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      inter: ['Inter Variable', 'sans-serif'],
+      ibm: ['IBM Plex Sans', 'sans-serif'],
+      serif: ['Newsreader Variable', 'serif'],
+    },
     colors: {
-      'black-bg': '#111111',
-      'white-text': '#E5E5E5',
-      'grey-text': '#a9a9a9',
+      'black-custom': '#111111',
+      'white-custom': '#E5E5E5',
+      'grey-custom': '#a9a9a9',
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
