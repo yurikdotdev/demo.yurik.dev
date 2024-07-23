1d8eec0b31b305ce4305ab0284329b6624f8444d
yurik-dev <172495341+yurik-dev@users.noreply.github.com>
2024-07-23 16:52:24 +0700

    fix: tailwind font typo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -8,7 +6,7 @@ export default {
     fontFamily: {
       inter: ['Inter Variable', 'sans-serif'],
       ibm: ['IBM Plex Sans', 'sans-serif'],
-      serif: ['Newsreader Variable', 'serif'],
+      news: ['Newsreader Variable', 'serif'],
     },
     colors: {
       'black-custom': '#111111',
